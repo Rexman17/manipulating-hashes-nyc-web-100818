8894e4ea981b0c432f27806d49395355168010be
github username <github email address>
2018-09-20 10:31:08 -0400

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,8 +1,10 @@
+require 'pry' # u can only access what's above pry
+
 def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -12,11 +14,29 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
+# binding.pry
+#   #your code here
+#   contacts.each do |person, data|
+#
+#     data.each do |attribute, value|
+#
+#       if attribute == :favorite_icecream_flavors
+#         value.delete_if {|x| x == "strawberry"}
+#       end
+#     end
+#   end
 
-  #your code here
+# go directly to the hash w the key of Freddy Mercury since u don't need Jon Snow - starts iteration from Freddy Mercury
 
+  # contacts["Freddy Mercury"].each do |attribute, value|
+  #   if attribute == :favorite_icecream_flavors
+  #     value.delete_if {|x| x == "strawberry"}
+  #   end
+  # end
+
+# most specific
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if {|x| x == "strawberry"}
 
   #remember to return your newly altered contacts hash!
   contacts
 end
-
